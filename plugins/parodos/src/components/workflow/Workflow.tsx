271db00d74128f70fc2a0a3c6626a7db6293aa98
Paul Cowan <paul.cowan@cutting.scot>
2023-06-14 16:55:48 +0100

    remove debug code

--- plugins/parodos/src/components/workflow/Workflow.tsx
+++ plugins/parodos/src/components/workflow/Workflow.tsx
@@ -129,7 +129,7 @@ export function Workflow(): JSX.Element {
                   <Grid item xs={7} xl={5}>
                     <ProgressBar
                       variant="circular"
-                      value={complete ? 67 : workflowProgress ?? 1}
+                      value={complete ? 100 : workflowProgress ?? 1}
                     />
                   </Grid>
                 )}
