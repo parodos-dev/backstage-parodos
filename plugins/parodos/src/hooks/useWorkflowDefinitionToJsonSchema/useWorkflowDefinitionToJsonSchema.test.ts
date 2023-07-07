a88939bffc7660abeb5a11abbead6cfeb26c5a66
Paul Cowan <paul.cowan@cutting.scot>
2023-07-07 15:30:49 +0100

    update nested schemas

--- plugins/parodos/src/hooks/useWorkflowDefinitionToJsonSchema/useWorkflowDefinitionToJsonSchema.test.ts
+++ plugins/parodos/src/hooks/useWorkflowDefinitionToJsonSchema/useWorkflowDefinitionToJsonSchema.test.ts
@@ -1,62 +1,66 @@
 import { reducer } from './useWorkflowDefinitionToJsonSchema';
 import get from 'lodash.get';
+import { WorkflowDefinition } from '../../models/workflowDefinitionSchema';
+import { jsonSchemaFromWorkflowDefinition } from './jsonSchemaFromWorkflowDefinition';
 
 type State = Parameters<typeof reducer>[0];
 type Actions = Parameters<typeof reducer>[1];
 
-const state = {
-  formSchema: {
-    steps: [
-      {
-        schema: {
-          type: 'object',
-          title: 'Complex Work Flow',
-          properties: {
-            complexWorkFlow: {
-              type: 'object',
-              properties: {
-                WORKFLOW_SELECT_SAMPLE: {
-                  title: 'WORKFLOW_SELECT_SAMPLE',
-                  type: 'string',
-                  enum: ['option1', 'option2'],
-                  valueProviderName: 'complexWorkFlowValueProvider',
-                },
-                WORKFLOW_MULTI_SELECT_SAMPLE: {
-                  title: 'WORKFLOW_MULTI_SELECT_SAMPLE',
-                  type: 'array',
-                  uniqueItems: true,
-                  items: {
-                    type: 'string',
-                    enum: [],
-                  },
-                },
-              },
-            },
-          },
-          required: ['complexWorkFlow'],
-        },
-        uiSchema: {
-          complexWorkFlow: {
-            'ui:hidden': true,
-            WORKFLOW_SELECT_SAMPLE: {
-              'ui:help': 'Workflow select parameter sample',
-              'ui:original-format': 'select',
-            },
-            WORKFLOW_MULTI_SELECT_SAMPLE: {
-              'ui:widget': 'checkboxes',
-              'ui:help': 'Workflow multi-select parameter sample',
-              'ui:original-format': 'multi-select',
-            },
-          },
-        },
-        title: 'Complex Work Flow',
-      },
-    ],
+const workflowDefinition: WorkflowDefinition = {
+  id: 'ae9ebbb0-7418-42bb-91ca-1c1da0e430de',
+  name: 'complexWorkFlow',
+  type: 'INFRASTRUCTURE',
+  processingType: 'SEQUENTIAL',
+  author: null,
+  createDate: '2023-07-07T07:06:59.130+00:00',
+  modifyDate: '2023-07-07T07:06:59.130+00:00',
+  parameters: {
+    projectUrl: {
+      format: 'uri',
+      description: 'The project url',
+      type: 'string',
+      required: false,
+    },
+    WORKFLOW_SELECT_SAMPLE: {
+      valueProviderName: 'complexWorkFlowValueProvider',
+      format: 'select',
+      description: 'Workflow select parameter sample',
+      type: 'string',
+      required: false,
+      enum: ['option1', 'option2'],
+    },
+    WORKFLOW_MULTI_SELECT_SAMPLE: {
+      format: 'multi-select',
+      description: 'Workflow multi-select parameter sample',
+      type: 'string',
+      required: false,
+    },
+    workloadId: {
+      format: 'text',
+      description: 'The workload id',
+      type: 'string',
+      required: true,
+    },
+    DYNAMIC_TEXT_SAMPLE: {
+      format: 'text',
+      description: 'dynamic text sample',
+      type: 'string',
+      required: false,
+    },
   },
-  initialized: true,
-} as unknown as State;
+  works: [],
+};
 
-const action = {
+const formSchema = jsonSchemaFromWorkflowDefinition(workflowDefinition, {
+  steps: [],
+});
+
+const state: State = {
+  formSchema,
+  initialized: true,
+};
+
+const action: Actions = {
   type: 'UPDATE_SCHEMA',
   payload: {
     valueProviderResponse: [
@@ -68,12 +72,10 @@ const action = {
       },
     ],
   },
-} as Actions;
+};
 
-// TODO: DO NOT merge without this
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip('reducer', () => {
-  it('updates the schema with the valueProviderRespose', () => {
+describe('reducer', () => {
+  it('updates the schema with the valueProviderResponse', () => {
     const updateSchma = jest.fn();
     const enumPath =
       'formSchema.steps[0].schema.properties.complexWorkFlow.properties.WORKFLOW_MULTI_SELECT_SAMPLE.items.enum';
