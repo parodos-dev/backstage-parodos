bd35b7463920965586d14aa1ca752b452c309a2c
Dmitriy Lazarev <w@kich.dev>
2023-08-03 15:13:36 +0400

    Skip reading storage state on CI
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- packages/e2e/tests/sessionStorage.ts
+++ packages/e2e/tests/sessionStorage.ts
@@ -4,14 +4,18 @@ import { STORAGE_STATE } from '../playwright.config';
 
 export const test = base.extend({
   context: async ({ context }, use) => {
-    const sessionStorage = JSON.parse(fs.readFileSync(STORAGE_STATE, 'utf-8'));
+    if (!process.env.CI) {
+      const sessionStorage = JSON.parse(
+        fs.readFileSync(STORAGE_STATE, 'utf-8'),
+      );
 
-    await context.addInitScript(storage => {
-      for (const [key, value] of Object.entries(JSON.parse(storage))) {
-        // @ts-ignore
-        window.sessionStorage.setItem(key, value);
-      }
-    }, sessionStorage);
+      await context.addInitScript(storage => {
+        for (const [key, value] of Object.entries(JSON.parse(storage))) {
+          // @ts-ignore
+          window.sessionStorage.setItem(key, value);
+        }
+      }, sessionStorage);
+    }
 
     use(context);
   },
